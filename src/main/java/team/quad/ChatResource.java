c0f584f6fee7d70a5e747a65a94fd92643b61f5c
Uanderson Soares <x@usoar.es>
2019-05-09 16:56:49 +0200

    Add a basic UI for chatting

--- src/main/java/team/quad/ChatResource.java
+++ src/main/java/team/quad/ChatResource.java
@@ -4,8 +4,7 @@ import io.vertx.axle.core.eventbus.EventBus;
 import io.vertx.axle.core.eventbus.Message;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-
-import java.util.concurrent.CompletionStage;
+import org.reactivestreams.Publisher;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
@@ -17,58 +16,57 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-import org.reactivestreams.Publisher;
+import java.util.concurrent.CompletionStage;
 
 @Path("/")
 public class ChatResource {
 
-    @Inject
-    EventBus eventBus;
+  @Inject
+  EventBus eventBus;
 
-    @POST
-    @Path("join")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public CompletionStage<User> join(@Valid User user) {
-        return eventBus.<JsonObject>send("join", JsonObject.mapFrom(user))
-                .thenApply(Message::body)
-                .thenApply(jsonObject -> jsonObject.mapTo(User.class));
-    }
+  @POST
+  @Path("join")
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  public CompletionStage<User> join(@Valid User user) {
+    return eventBus.<JsonObject>send("join", JsonObject.mapFrom(user))
+      .thenApply(Message::body)
+      .thenApply(jsonObject -> jsonObject.mapTo(User.class));
+  }
 
-    @PUT
-    @Path("leave")
-    @Consumes(MediaType.TEXT_HTML)
-    public CompletionStage<String> leave(@Email String username) {
-        return eventBus.<String>send("leave", username)
-                .thenApply(Message::body)
-                .exceptionally(Throwable::getMessage);
-    }
+  @PUT
+  @Path("leave")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public CompletionStage<String> leave(@Email String username) {
+    return eventBus.<String>send("leave", username)
+      .thenApply(Message::body)
+      .exceptionally(Throwable::getMessage);
+  }
 
-    @GET
-    @Path("members")
-    @Produces(MediaType.APPLICATION_JSON)
-    public CompletionStage<JsonArray> members() {
-        return eventBus.<JsonArray>send("get.members", "")
-                .thenApply(Message::body);
-    }
+  @GET
+  @Path("members")
+  @Produces(MediaType.APPLICATION_JSON)
+  public CompletionStage<JsonArray> members() {
+    return eventBus.<JsonArray>send("get.members", "")
+      .thenApply(Message::body);
+  }
 
-    @POST
-    @Produces(MediaType.TEXT_HTML)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Path("chat")
-    public CompletionStage<String> chat(@Valid ChatMessage message) {
-        return eventBus.<String>send("chat", JsonObject.mapFrom(message))
-                .thenApply(Message::body)
-                .exceptionally(Throwable::getMessage);
-    }
+  @POST
+  @Produces(MediaType.TEXT_HTML)
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Path("chat")
+  public CompletionStage<String> chat(@Valid ChatMessage message) {
+    return eventBus.<String>send("chat", JsonObject.mapFrom(message))
+      .thenApply(Message::body)
+      .exceptionally(Throwable::getMessage);
+  }
 
-    @GET
-    @Produces(MediaType.SERVER_SENT_EVENTS)
-    @Path("stream")
-    public Publisher<String> stream() {
-        return eventBus.<String>consumer("stream").toPublisherBuilder()
-                .map(Message::body)
-                .buildRs();
-    }
+  @GET
+  @Produces(MediaType.SERVER_SENT_EVENTS)
+  @Path("stream")
+  public Publisher<String> stream() {
+    return eventBus.<String>consumer("stream").toPublisherBuilder()
+      .map(Message::body)
+      .buildRs();
+  }
 }
--- src/main/resources/META-INF/resources/streaming.js
+++ src/main/resources/META-INF/resources/streaming.js
@@ -1,7 +1,137 @@
-var eventSource = new EventSource("/stream");
-eventSource.onmessage = function (event) {
-    var container = document.getElementById("container");
-    var paragraph = document.createElement("p");
-    paragraph.innerHTML = event.data;
-    container.appendChild(paragraph);
-};
+(function () {
+  const messageTemplate = document.createElement('template');
+  messageTemplate.innerHTML = `<div class="message"></div>`;
+
+  const membersTemplate = document.createElement('template');
+  messageTemplate.innerHTML = `
+    <div class="message">
+      <div class="members"></div>    
+    </div>
+  `;
+
+  const messageList = document.getElementById('message-list');
+  const chatInput = document.getElementById('chat-input');
+  let eventSource;
+
+  function tryExecutingCommand(message) {
+    const parts = message.split(' ');
+
+    switch (parts[0]) {
+      case '/join': {
+        executeJoin(parts.splice(1));
+        break;
+      }
+
+      case '/leave': {
+        executeLeave();
+        break;
+      }
+
+      case '/members': {
+        executeMembers();
+        break;
+      }
+
+      default: {
+        executeChat(message);
+      }
+    }
+  }
+
+  function executeJoin(parts) {
+    localStorage.setItem('quad.chat.username', parts[0]);
+    localStorage.setItem('quad.chat.alias', parts[1]);
+
+    fetchMethod('/join', 'POST', {
+      username: parts[0],
+      alias: parts[1]
+    })
+      .then(response => {
+        eventSource = new EventSource("/stream");
+        eventSource.onmessage = event => {
+          console.log(event);
+          appendMessage(event.data);
+        };
+
+        chatInput.value = '';
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  function executeLeave() {
+    const username = localStorage.getItem('quad.chat.username');
+    fetchMethod(`/leave?username=${username}`, 'PUT')
+      .then(response => {
+        localStorage.removeItem('quad.chat.username');
+        localStorage.removeItem('quad.chat.alias');
+        eventSource.close();
+        chatInput.value = '';
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  function executeMembers() {
+    fetchMethod('/members', 'GET')
+      .then(response => response.json())
+      .then(members => {
+        const message = messageTemplate.content.cloneNode(true);
+        const div = message.querySelector('.members');
+        members.forEach(member => {
+          const elMember = document.createElement('div');
+          elMember.appendChild(document.createTextNode(member));
+          div.appendChild(elMember);
+        });
+
+        appendMessageElement(message);
+        chatInput.value = '';
+      });
+  }
+
+  function executeChat(message) {
+    fetchMethod('/chat', 'POST', {
+      username: localStorage.getItem('quad.chat.username'),
+      message: message
+    })
+      .then(response => {
+        chatInput.value = '';
+      });
+  }
+
+  function appendMessage(value) {
+    const message = messageTemplate.content.cloneNode(true);
+    const div = message.querySelector('div');
+    div.appendChild(document.createTextNode(value));
+    appendMessageElement(message);
+  }
+
+  function appendMessageElement(message) {
+    messageList.appendChild(message);
+  }
+
+  function fetchMethod(path, method, body) {
+    const url = window.location.href.endsWith('/')
+      ? window.location.href + path.substring(1)
+      : window.location.href + path;
+
+    return fetch(url, {
+      method: method,
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
+  chatInput.addEventListener('keyup', event => {
+    event.code === 'Enter' && tryExecutingCommand(chatInput.value);
+  });
+
+  executeJoin([
+    localStorage.getItem('quad.chat.username'),
+    localStorage.getItem('quad.chat.alias')
+  ]);
+})();
